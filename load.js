11199859f2efec014a49ee75ec91d0989411f850
Richard Rodger <richard@ricebridge.com>
2023-06-29 14:03:50 +0100

    deps

--- load.js
+++ load.js
@@ -2,11 +2,44 @@ const Seneca = require('seneca')
 
 
 const seneca = Seneca({legacy:false})
+      // .test('print')
       .test()
+
       .use('promisify')
       .use('entity')
-      .use('gateway')
+      .use('entity-util')
+      .use('entity-history')
+      .use('mem-store')
+      // .use('gateway')
+      // .use('gateway-auth')
+      // .use('gateway-express')
+      // .use('gateway-lambda')
       .use('flow')
+      .use('cache')
+      .use('provider')
+      // .use('user')
+      .use('ledger')
+      .use('allow')
+      // .use('env')
+      .use('registry')
+      .use('request')
+      .use('mail')
+      .use('refer')
+      .use('member')
+      .use('group')
+
+      // .use('vote')
+      // .use('owner')
+
+
+      .use('kv')
+      .use('reload')
+      // .use('repl')
+      .use('doc')
+
       .ready(function() {
         console.log('READY')
+
+        // TODO: does repl close?
+        this.close()
       })
